4bbd2b057145b05e8ffc27c96bae9b9c4ddfbbfa
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-04-14 12:02:01 -0500

    Git and SVN 1.7 only have .git/.svn directories in the top level of the repository, so always look at the repository root for VCS metadata.

--- cmake/modules/VersionFromVCS.cmake
+++ cmake/modules/VersionFromVCS.cmake
@@ -1,28 +1,28 @@
 # Adds version control information to the variable VERS. For
 # determining the Version Control System used (if any) it inspects the
-# existence of certain subdirectories under CMAKE_CURRENT_SOURCE_DIR.
+# existence of certain subdirectories under LLVM_MAIN_SRC_DIR.
 
 function(add_version_info_from_vcs VERS)
   set(result ${${VERS}})
-  if( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.svn" )
+  if( EXISTS "${LLVM_MAIN_SRC_DIR}/.svn" )
     set(result "${result}svn")
     # FindSubversion does not work with symlinks. See PR 8437
-    if( NOT IS_SYMLINK "${CMAKE_CURRENT_SOURCE_DIR}" )
+    if( NOT IS_SYMLINK "${LLVM_MAIN_SRC_DIR}" )
       find_package(Subversion)
     endif()
     if( Subversion_FOUND )
-      subversion_wc_info( ${CMAKE_CURRENT_SOURCE_DIR} Project )
+      subversion_wc_info( ${LLVM_MAIN_SRC_DIR} Project )
       if( Project_WC_REVISION )
         set(result "${result}-r${Project_WC_REVISION}")
       endif()
     endif()
-  elseif( EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.git )
+  elseif( EXISTS ${LLVM_MAIN_SRC_DIR}/.git )
     set(result "${result}git")
     # Try to get a ref-id
     find_program(git_executable NAMES git git.exe git.cmd)
     if( git_executable )
       execute_process(COMMAND ${git_executable} show-ref HEAD
-                      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                      WORKING_DIRECTORY ${LLVM_MAIN_SRC_DIR}
                       TIMEOUT 5
                       RESULT_VARIABLE git_result
                       OUTPUT_VARIABLE git_output)
@@ -31,7 +31,7 @@ function(add_version_info_from_vcs VERS)
         set(result "${result}-${git_ref_id}")
       else()
         execute_process(COMMAND ${git_executable} svn log --limit=1 --oneline
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                        WORKING_DIRECTORY ${LLVM_MAIN_SRC_DIR}
                         TIMEOUT 5
                         RESULT_VARIABLE git_result
                         OUTPUT_VARIABLE git_output)
